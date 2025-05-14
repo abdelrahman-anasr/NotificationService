b23a7cd03f48202a763cbbbbeda1cdcd7de11dbe
abdelrahman-anasr <abdulrahman.nasr@student.giu-uni.de>
2025-05-15 02:44:56 +0300

    updated

--- src/notificationService.ts
+++ src/notificationService.ts
@@ -151,7 +151,7 @@ import { create } from "domain";
 
     const app = express() as any;
     var corsOptions = {
-        origin : "http://localhost:4000/user",
+        origin : "http://localhost:3000",
         credentials: true
     }
     app.use(cors(corsOptions));
